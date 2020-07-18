2e37f0f70f77453dd91a69d1d85766a6841e561a
gondiaz <gonzalodiazlopez10@gmail.com>
2020-07-18 11:14:57 +0200

    Add docstring to photon-simulation

--- invisible_cities/cities/detsim_simulate_photons.py
+++ invisible_cities/cities/detsim_simulate_photons.py
@@ -8,7 +8,13 @@ from typing import Callable
 #######################################
 def generate_s1_photons(ws       : float,
                         energies : np.ndarray) -> np.ndarray:
-    """generate s1 photons"""
+    """generates s1 photons from the energy hits
+
+    :ws: float
+        scintillation yield
+    :energies: np.ndarray
+        energy hits
+    """
     return np.random.poisson(energies / ws)
 
 
@@ -17,6 +23,13 @@ def generate_s2_photons(el_gain        : float,
                         nes            : np.ndarray) -> np.ndarray:
     """generate number of EL-photons produced by secondary electrons that reach
     the EL (after drift and diffusion)
+
+    :el_gain: float
+        number of phtotons emitted per ionization electron at EL
+    :el_gain_sigma: float
+        standard deviation of the el_gain.
+    :nes: np.ndarray
+        number of ionization electrons at arriving at some EL X,Y position.
     """
     nphs = np.random.normal(el_gain*nes, np.sqrt(el_gain_sigma**2*nes))
     return nphs
