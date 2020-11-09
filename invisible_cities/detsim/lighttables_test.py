b8e3d670fa6889806abffedd6eb32c9fa8c6f4c7
mmkekic <mmkekic@gmail.com>
2020-11-09 23:28:03 +0100

    Add test for reading optional arguments
    Check that el_gap and active_r are properly read from the files or
    from the keywords argument, raising the UserWarning in case of mismatch

---
+++ invisible_cities/detsim/lighttables_test.py
@@ -0,0 +1,43 @@
+import os
+import pytest
+
+import numpy as np
+
+from pytest import warns
+
+from .. database.load_db import DataSiPM
+from .. io      .dst_io  import load_dst
+
+from . lighttables import LT
+from . lighttables import LT_SiPM
+from . lighttables import LT_PMT
+
+from hypothesis                import given
+from hypothesis                import settings
+from hypothesis.strategies     import floats
+from hypothesis.strategies     import integers
+
+@pytest.fixture(scope='module')
+def get_dfs(ICDATADIR):
+    psffname = os.path.join(ICDATADIR, 'NEXT_NEW.tracking.S2.SiPM.LightTable.h5')
+    ltfname  = os.path.join(ICDATADIR, 'NEXT_NEW.energy.S2.PmtR11410.LightTable.h5')
+    psf_df   = load_dst(psffname, 'PSF', 'LightTable').set_index('dist_xy')
+    lt_df    = load_dst( ltfname,  'LT', 'LightTable').set_index(['x', 'y'])
+    psf_conf = load_dst(psffname, 'PSF', 'Config'    ).set_index('parameter')
+    lt_conf  = load_dst( ltfname,  'LT', 'Config'    ).set_index('parameter')
+    return  {'psf':(psffname, psf_df, psf_conf),
+             'lt' : (ltfname, lt_df, lt_conf)}
+
+
+def test_LT_SiPM_optional_arguments(get_dfs):
+    datasipm = DataSiPM('new')
+    fname, psf_df, psf_conf = get_dfs['psf']
+    LT = LT_SiPM(fname=fname, sipm_database=datasipm)
+    #check the values are read from teh table
+    assert LT.el_gap   == psf_conf.loc['EL_GAP'    ].astype(float).value
+    assert LT.active_r == psf_conf.loc['ACTIVE_rad'].astype(float).value
+    #check optional arguments are set with User Warning
+    with warns(UserWarning):
+        LT = LT_SiPM(fname=fname, sipm_database=datasipm, el_gap=2, active_r=150)
+        assert LT.el_gap   == 2
+        assert LT.active_r == 150
