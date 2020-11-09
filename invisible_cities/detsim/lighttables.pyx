d3bfc4396c53e4bf001047ea1d176766aa08f4fa
mmkekic <mmkekic@gmail.com>
2020-11-09 21:22:47 +0100

    Add LT class for PSF on SiPMs

--- invisible_cities/detsim/lighttables.pyx
+++ invisible_cities/detsim/lighttables.pyx
@@ -4,6 +4,9 @@ import warnings
 
 cimport cython
 cimport numpy as np
+from libc.math cimport sqrt
+from libc.math cimport round as cround
+from libc.math cimport floor
 
 
 from ..core import system_of_units as units
@@ -57,4 +60,69 @@ def extract_info_lighttables_(fname, group_name, el_gap, active_r):
         active_r = active_r_f
 
     return lt_df, config_df, el_gap, active_r
+
+cdef class LT_SiPM(LT):
+    cdef readonly:
+        double [:] snsx
+        double [:] snsy
+        double el_gap
+        double active_r
+    cdef:
+        double [:, ::1] values
+        double psf_bin
+        double el_bin
+        double max_zel
+        double max_psf
+        double max_psf2
+        double inv_bin
+        double active_r2
+
+    def __init__(self, *, fname, sipm_database, el_gap=None, active_r=None):
+        lt_df, config_df, el_gap, active_r = extract_info_lighttables_(fname, 'PSF', el_gap, active_r)
+        lt_df.set_index('dist_xy', inplace=True)
+        self.el_gap   = el_gap
+        self.active_r = active_r
+        self.active_r2 = active_r**2
+
+        el_pitch = float(config_df.loc["pitch_z"].value) * units.mm
+        self.zbins_ = np.arange(el_pitch/2., el_gap+np.finfo(float).eps, el_pitch).astype(np.double)
+
+        self.values = np.array(lt_df.values/len(self.zbins_), order='C', dtype=np.double)
+        self.psf_bin    = float(lt_df.index[1]-lt_df.index[0])
+        self.inv_bin = 1./self.psf_bin
+        self.el_bin = el_pitch
+
+        self.snsx        = sipm_database.X.values.astype(np.double)
+        self.snsy        = sipm_database.Y.values.astype(np.double)
+        self.sensor_ids_ = sipm_database.index.values.astype(np.intc)
+        self.max_zel     = el_gap
+        self.max_psf     = max(lt_df.index.values)
+        self.max_psf2    = self.max_psf**2
+
+    @cython.boundscheck(False)
+    @cython.wraparound(False)
+    @cython.cdivision(True)
+    @cython.initializedcheck(False)
+    cdef double* get_values_(self, const double x, const double y, const int sns_id):
+        cdef:
+            double dist
+            double aux
+            unsigned int psf_bin_id
+            double xsipm
+            double ysipm
+            double tmp_x
+            double tmp_y
+            double*  values
+        xsipm = self.snsx[sns_id]
+        ysipm = self.snsy[sns_id]
+        tmp_x = x-xsipm; tmp_y = y-ysipm
+        dist = tmp_x*tmp_x + tmp_y*tmp_y
+        if dist>self.max_psf2:
+            return NULL
+        if x*x+y*y>=self.active_r2:
+            return NULL
+        aux = sqrt(dist)*self.inv_bin
+        bin_id = <int> floor(aux)
+        values = &self.values[bin_id, 0]
+        return values
 
